8be2043044de7568e49515bee731833129c57930
Matthew <57062385+TheMatthattanProject@users.noreply.github.com>
2022-11-28 13:20:47 +1100

    Add table entries
     - Add data to end of table array
     - See #4 for business logic

--- base-app/src/screens/TableScreen.js
+++ base-app/src/screens/TableScreen.js
@@ -14,10 +14,21 @@ export function TableScreen() {
     return
   }
 
+  const addTableEntry = () => 
+  {
+    // Adding deep-copied version of the first element to the end
+    setTableEntries((currentArray) => 
+    {
+        return currentArray.concat([JSON.parse(JSON.stringify(currentArray[0]))])
+    })
+  }
+
 
   return (
     <div>
       <button onClick={async (event) => {fetchTableData()}}>Load</button>
+      <button onClick={() => {addTableEntry()}}>Add</button>
+      <button onClick={() => {console.log(tableEntries)}}>Print</button>
     </div>
   );
 }
