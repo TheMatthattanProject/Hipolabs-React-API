5efb22009f525452f111e901fcf7289b501f7f06
Matthew <57062385+TheMatthattanProject@users.noreply.github.com>
2022-11-28 13:08:46 +1100

    Fetch data from API
    API_ENDPOINT: http://universities.hipolabs.com/search?country=Australia
    
    Fetched JSON data to be used within base app.
     - Completes #2

--- base-app/src/App.js
+++ base-app/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import { TableScreen } from './screens/TableScreen';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <TableScreen/>
         
         
       </header>
